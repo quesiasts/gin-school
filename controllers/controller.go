abdf8a46b48fef94ab8b77b9a32ec33911089524
--add <quesia.santos@olist.com>
2022-07-06 18:52:20 -0300

    add assets/routes/templates

--- controllers/controller.go
+++ controllers/controller.go
@@ -36,3 +36,64 @@ func ListAllStudents(c *gin.Context) {
 	database.DB.Find(&students)
 	c.JSON(200, students)
 }
+
+func SearchForID(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"Not Found": "Student not found"})
+		return
+	}
+	c.JSON(http.StatusOK, student)
+}
+
+func DeleteStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.Delete(&student, id)
+	c.JSON(http.StatusOK, gin.H{"data": "Removed student with success"})
+}
+
+func EditStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := models.ValidateStudents(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, student)
+}
+
+func SearchForDocument(c *gin.Context) {
+	var student models.Student
+	document := c.Param("document")
+	database.DB.Where(&models.Student{Document: document}).First(&student)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"Not Found": "Student not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
+}
+
+func ShowIndexPage(c *gin.Context) {
+	var students []models.Student
+	database.DB.Find(&students)
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"students": students,
+	})
+}
+
+func RouteNotFound(c *gin.Context) {
+	c.HTML(http.StatusNotFound, "404.html", nil)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,16 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
 	r.GET("/:name", controllers.Introduction)
+	r.POST("/students", controllers.CreateNewStudent)
+	r.GET("/students", controllers.ListAllStudents)
+	r.GET("/students/:id", controllers.SearchForID)
+	r.DELETE("/students/:id", controllers.DeleteStudent)
+	r.PATCH("/students/:id", controllers.EditStudent)
+	r.GET("/students/document/:document", controllers.SearchForDocument)
+	r.GET("/index", controllers.ShowIndexPage)
+	r.NoRoute(controllers.RouteNotFound)
 	r.Run()
 }
